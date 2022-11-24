7d52a5331bad2492ac68b0d5efb9d1dd5a9d4455
Graciele89 <gracieleludwig@yahoo.com.br>
2022-11-24 15:43:14 +0000

    terst

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App Graci 2!</Text>
+      <Text>Open up App Graci 4!</Text>
       <StatusBar style="auto" />
     </View>
   );
