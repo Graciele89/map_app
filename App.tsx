8c05167af9e01e6e3e2015283376935fcd88ff09
Graciele89 <gracieleludwig@yahoo.com.br>
2022-11-29 22:45:39 +0000

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,156 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, { useRef, useState } from 'react';
+import MapView, { LatLng, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { StyleSheet, View, Dimensions, Text, Touchable, TouchableOpacity } from 'react-native';
+import { GooglePlaceDetail, GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_API_KEY } from './environments';
+import Constants from "expo-constants";
+//import { useRef, useState } from "react";
+import MapViewDirections from 'react-native-maps-directions';
+
+
+// https://docs.expo.dev/versions/latest/sdk/map-view/
+// https://www.npmjs.com/package/react-native-google-places-autocomplete
+// https://www.npmjs.com/package/react-native-maps-directions
+
+
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+latitude: 53.3498019,           // location where i got the cordinates:
+longitude: -6.260254172727272,  // https://www.maps.ie/coordinates.html
+latitudeDelta: LATITUDE_DELTA,
+longitudeDelta: LONGITUDE_DELTA,
+};
+
+type InputAutocompleteProps ={
+  label: string;
+  placeholder: string;
+  onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+};
+
+function InputAutocomplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}: InputAutocompleteProps) {
+  return(
+    <>
+    <Text>{label}</Text>
+    <GooglePlacesAutocomplete
+    styles={{ textInput: styles.input }}
+    placeholder={placeholder || ""} 
+    fetchDetails
+    onPress={(data, details = null) => {
+      onPlaceSelected(details);
+    }}
+    query={{
+      key: GOOGLE_API_KEY,
+      language: "en"
+    }}
+    />
+  </>
+  )
+}
 
 export default function App() {
-  return (
+const [origin, setOrigin] = useState<LatLng | null>()
+const [destination, setDestination] = useState<LatLng | null>()
+const [showDirections, setShowDirections] = useState(false)
+const [ distance, setDistance] = useState(0);
+const [duration, setDuration] = useState(0);
+const mapRef = useRef<MapView>(null);
+
+// to move the camera to place selected
+const moveTo = async(position: LatLng) => {
+  const camera = await mapRef.current?.getCamera()
+  if (camera){
+    camera.center = position;
+    mapRef.current?.animateCamera(camera, {duration: 1000}) //one second
+  }
+};
+
+const edgePaddingValue = 75;
+
+const edgePadding = {
+  top: edgePaddingValue,
+  right: edgePaddingValue,
+  bottom: edgePaddingValue,
+  left: edgePaddingValue,
+};
+
+const traceRouteOnReady = (args: any) => {
+if (args){
+    setDistance(args.distance)
+    setDuration(args.duration)
+  }
+}
+
+const traceRoute = () => {
+  if (origin && destination) {
+    setShowDirections(true)
+    mapRef.current?.fitToCoordinates([origin, destination], {edgePadding})
+  }
+}
+
+const onPlaceSelected = (
+  details:GooglePlaceDetail | null,
+   flag: "origin" | "destination"
+   ) => {
+    const set = flag === "origin" ? setOrigin : setDestination
+    const position = {
+      latitude: details?.geometry.location.lat || 0,
+      longitude: details?.geometry.location.lng || 0
+    }
+    set (position);
+    moveTo(position);
+};
+return (
     <View style={styles.container}>
-      <Text>Open up App Graci 4!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      ref={mapRef}
+       style={styles.map} 
+       provider={PROVIDER_GOOGLE} 
+       initialRegion={INITIAL_POSITION} 
+       >
+        {origin && <Marker coordinate={origin} /> }
+        {destination && <Marker coordinate={destination} />}
+        {showDirections && origin && destination && (
+        <MapViewDirections
+        origin={origin}
+        destination={destination}
+        apikey={GOOGLE_API_KEY}
+        strokeColor="#6cf542"
+        strokeWidth={5}
+        onReady={traceRouteOnReady}//show distance and duration from origin to destination
+      />
+      ) }
+        
+      </MapView>
+       <View style={styles.searchContainer}>
+        <InputAutocomplete
+        label="Origin"
+        onPlaceSelected={(details) => {
+          onPlaceSelected(details, "origin");
+        } } placeholder={''}         />
+        <InputAutocomplete
+        label="Destination"
+        onPlaceSelected={(details) => {
+          onPlaceSelected(details, "destination");
+        } } placeholder={''}      />
+     <TouchableOpacity style={styles.button} onPress={traceRoute}>
+          <Text style={styles.buttonText}>Trace Route</Text>
+         </TouchableOpacity>
+         {distance && duration ? (
+         <View>
+          <Text>Distance: {distance.toFixed(2)}</Text>
+          <Text>Duration: {Math.ceil(duration)} min</Text>
+         </View>
+         ): null} 
+       </View>
     </View>
   );
 }
@@ -13,8 +158,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+  // applying some style to the search box
+  searchContainer: {
+    position: "absolute",
+    width: "90%",
+    backgroundColor: "white",
+    shadowColor: "Black",
+    shadowOffset: { width: 2, height: 2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 5,   //android
+    elevation: 5,     // ios
+    padding: 5,
+    borderRadius: 7,
+    top: Constants.statusBarHeight,
+  },
+  input: {
+    borderColor: "#888",
+    borderWidth: 1,
+  },
+  button: {
+    backgroundColor: "#bbb",
+    paddingVertical: 13,
+    marginTop: 10,
+    borderRadius: 7,
+  },
+  buttonText: {
+    textAlign: "center",
+  }
+
+
 });
+
+ 
+// on terminal instal de dependencies: npx expo install react-native-maps
